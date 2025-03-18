757a9dbc4452b03b91ef2ee42a7316ce452bdbbb
GioTFG <168219065+GioTFG@users.noreply.github.com>
2025-03-18 11:27:51 +0100

    Appunti sessioni con Flask Creato il file app.py. Scritta la secret key e la route per l'index.

--- Informatica/Flask project/appunti/sessioni/app.py
+++ Informatica/Flask project/appunti/sessioni/app.py
@@ -2,3 +2,9 @@ from flask import Flask, session, redirect, url_for, request, render_template_st
 
 app = Flask(__name__)
 app.secret_key = "provola"
+
+@app.route("/")
+def index():
+  if 'username' in session:
+    return f'Ciao, {session[session]}!</br></br><a href="{url_for("logout")}">Logout</a>'
+  return 'Non sei loggato.</br>Esegui il <a href="/login">login</a>'
