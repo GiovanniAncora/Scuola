0f7b1c8e16c51683077fe66c01aa8e96470b403f
GioTFG <168219065+GioTFG@users.noreply.github.com>
2025-03-18 11:32:44 +0100

    Appunti sessioni con Flask Creato la route per il login.

--- Informatica/Flask project/appunti/sessioni/app.py
+++ Informatica/Flask project/appunti/sessioni/app.py
@@ -8,3 +8,19 @@ def index():
   if 'username' in session:
     return f'Ciao, {session[session]}!</br></br><a href="{url_for("logout")}">Logout</a>'
   return 'Non sei loggato.</br>Esegui il <a href="/login">login</a>'
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username == 'admin' and password == 'pawssord':
+      session['username'] = username
+      flash('Login effettuato con successo.', 'success')
+      return redirect(url_for('index'))
+    else:
+      flash('Credenziali errate!', 'error')
+      return redirect(url_for('login'))
+    
+  return render_template_string(LOGIN_TEMPLATE)
