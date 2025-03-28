8eed71ae9631363e493f5cab5207361c3cb5191c
Giovanni Ancora <ancora.giovanni.scuola+github@gmail.com>
2025-03-28 12:19:04 +0100

    Aggiunta logica per le sessioni.

--- Informatica/Flask project/db5CI/app.py
+++ Informatica/Flask project/db5CI/app.py
@@ -1,13 +1,16 @@
 import pymysql.cursors
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 
 conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='24082006', database='db5CI')
 cursore = conn.cursor()
 
-cursore.execute('SHOW COLUMNS FROM verifiche;')
-print(cursore.fetchall())
+# cursore.execute('SHOW COLUMNS FROM verifiche;')
+# print(cursore.fetchall())
 
 app = Flask(__name__)
+
+app.secret_key = 'provola1234'
+
 
 @app.route('/')
 def index():
@@ -18,10 +21,13 @@ def index():
 
 @app.route('/voti/<studente>')
 def voti(studente):
-    queryVoti = 'SELECT * FROM verifiche WHERE studente = %s'
-    cursore.execute(queryVoti, (studente,))
-    risultatoVoti = cursore.fetchall()
-    return render_template("voti.html", voti=risultatoVoti, campi= cursore.description)
+    if 'studente' in session and studente == session['studente']:
+        queryVoti = 'SELECT * FROM verifiche WHERE studente = %s'
+        cursore.execute(queryVoti, (studente,))
+        risultatoVoti = cursore.fetchall()
+        return render_template("voti.html", voti=risultatoVoti, campi= cursore.description)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/medie')
 def medie():
@@ -30,5 +36,33 @@ def medie():
     risultatoMedie = cursore.fetchall()
     # print(risultatoMedie)
     return render_template("medie.html", medie=risultatoMedie, campi= cursore.description)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        studente = request.form['matricola']
+        codice = request.form['codice']
+
+        queryUtente = 'SELECT codice FROM alunni WHERE matricola=%s;'
+        cursore.execute(queryUtente, (studente,))
+        ris = cursore.fetchall()
+        print(ris)
+        if ris and ris[0][0] == codice:
+            session['studente'] = studente
+            session['codice'] = codice
+            # print(f'Login effettuato come {session["studente"]}: {session["codice"]}')
+            # print(type(session['studente']))
+            return redirect(url_for('voti', studente=studente))
+        else:
+            print('Login fallito.')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('studente', None)
+    session.pop('codice', None)
+    return redirect(url_for('index'))
 
 app.run(debug=True)
