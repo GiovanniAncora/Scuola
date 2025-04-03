b38ef9b912620680ea681d4913e5e6cf124a455f
Giovanni Ancora <ancora.giovanni.scuola+github@gmail.com>
2025-04-03 22:56:52 +0200

    Modificato tutto per gestire username a parte dalla matricola. Creata la pagina di registrazione.

--- Informatica/Flask project/db5CI/app.py
+++ Informatica/Flask project/db5CI/app.py
@@ -21,12 +21,26 @@ def index():
 
 @app.route('/voti/<studente>')
 def voti(studente):
-    if 'studente' in session and studente == session['studente']:
-        queryVoti = 'SELECT * FROM verifiche WHERE studente = %s'
-        cursore.execute(queryVoti, (studente,))
-        risultatoVoti = cursore.fetchall()
-        return render_template("voti.html", voti=risultatoVoti, campi= cursore.description)
+    if 'username' in session:
+        print(studente)
+
+        queryControlloMatr = "SELECT matr FROM utenti WHERE username = %s;"
+        cursore.execute(queryControlloMatr, (session['username'],))
+        matr = str(cursore.fetchall()[0][0])
+
+        print(type(studente), type(matr), studente == matr)
+        if studente == matr:
+            queryVoti = 'SELECT * FROM verifiche WHERE studente = %s'
+            cursore.execute(queryVoti, (studente,))
+            risultatoVoti = cursore.fetchall()
+            return render_template("voti.html", voti=risultatoVoti, campi= cursore.description)
+        else:
+            print('Non hai l\'accesso a QUESTA pagina.')
+            print(session['username'], matr)
+            return redirect(url_for('voti', studente=matr))
+
     else:
+        print('Fai l\'accesso')
         return redirect(url_for('login'))
 
 @app.route('/medie')
@@ -40,28 +54,62 @@ def medie():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        studente = request.form['matricola']
+        username = request.form['username']
         codice = request.form['codice']
 
-        queryUtente = 'SELECT codice FROM alunni WHERE matricola=%s;'
-        cursore.execute(queryUtente, (studente,))
+        queryUtente = 'SELECT * FROM utenti WHERE username=%s;'
+        cursore.execute(queryUtente, (username,))
         ris = cursore.fetchall()
         print(ris)
-        if ris and ris[0][0] == codice:
-            session['studente'] = studente
-            session['codice'] = codice
-            # print(f'Login effettuato come {session["studente"]}: {session["codice"]}')
-            # print(type(session['studente']))
-            return redirect(url_for('voti', studente=studente))
+        if ris:
+            if ris[0][1] == codice:
+                session['username'] = username
+                session['codice'] = codice
+                # print(f'Login effettuato come {session["username"]}: {session["codice"]}')
+                # print(type(session['username']))
+                return redirect(url_for('voti', studente= ris[0][2]))
+            else:
+                print('Password non corretta.')
+                return redirect(url_for('login'))
         else:
-            print('Login fallito.')
+            print('Username non esistente.')
             return redirect(url_for('login'))
 
     return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        matricola = request.form['matricola']
+
+        queryControllo = 'SELECT * FROM utenti WHERE username = %s;'
+        cursore.execute(queryControllo, (username,))
+        ris = cursore.fetchall()
+
+        if ris:
+            print("L'username esiste già.")
+            return redirect(url_for('register'))
+        
+        queryControlloMatr = 'SELECT matricola FROM alunni WHERE matricola = %s;'
+        cursore.execute(queryControlloMatr, (matricola,))
+        
+        if not cursore.fetchall():
+            print('La matricola non esiste.')
+            return redirect(url_for('register'))
+        
+        queryInserimento = "INSERT INTO utenti VALUES (%s, %s, %s);"
+        cursore.execute(queryInserimento, (username, password, matricola))
+        conn.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
 @app.route('/logout')
 def logout():
-    session.pop('studente', None)
+    session.pop('username', None)
     session.pop('codice', None)
     return redirect(url_for('index'))
 
